9ddf5d3a3de28e399c10b2c0c6c910daefe61951
TajalTechnology <mrpresident1189@gmail.com>
2024-08-08 01:45:42 +0600

    cicd test

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
+const testItem = require('./routes/test');
 
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
 app.get('/items', getItems);
+app.get('/test', testItem);
 app.post('/items', addItem);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
